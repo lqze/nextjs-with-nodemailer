c3f8baeb84ced6e0604961c9049065104682e110
Caleb Fetzer <caleb.fetzer@gmail.com>
2020-08-16 14:12:53 +0800

    Initial commit

---
+++ pages/api/submit.js
@@ -0,0 +1,51 @@
+/**
+ * A simple method of demonstrating how you can use the Next.js
+ * internal API for 'server-less' functions. We export an async function.
+ * 
+ * We import the nodemailer package to handle SMTP transactional mail.
+ * You could pass in your desired environe
+ */
+import nodemailer from 'nodemailer'
+
+export default async(req, res) => {
+  
+  let testAccount = await nodemailer.createTestAccount();
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass, // generated ethereal password
+    },
+  });
+
+  if (req.method === 'POST') {
+
+    let info = await transporter.sendMail({
+      from: '"Fred Foo 👻" <foo@example.com>', // sender address
+      to: "bar@example.com, baz@example.com", // list of receivers
+      subject: "Hello ✔", // Subject line
+      text: "Hello world?", // plain text body
+      html: "<b>Hello world?</b>", // html body
+    });
+
+    // We can also extract the form variables from our req.body
+    const { 
+      email = '', 
+      name = '' 
+    } = req.body
+
+    res.json({message: 
+    `
+      Ethereal Mail Message ID: ${info.messageId}
+      Email from form: ${email}
+      Name from form: ${name}
+    `})
+
+  } else {
+    res.status(200).json({message: `Response from /api/submit.`})
+  }
+
+}
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,95 @@
 import Head from 'next/head'
+import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home(props) {
+
+  /**
+   * Define our useState hooks
+   */
+  const [formLoading, setFormLoading] = useState(false)
+  const [statusMessage, setStatusMessage] = useState('')
+
+  const handleSubmission = async (event) => {
+    event.preventDefault() // for AJAX form submission, e.g. prevent the page refresh on a form submit
+    
+    setFormLoading(true) // Show a "loading" status for our form element
+
+    // Retrieve our form data
+    const formData = {
+      "name": document.getElementById('emailFormName').value,
+      "email": document.getElementById('emailFormEmail').value,
+    }
+
+    // Send our data in JSON using the standard ES5 Fetch API
+    fetch('/api/submit', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData),
+    })
+    .then(res => res.json()) // Transform our returned data into JSON
+    .then(data => { 
+      if (data) setStatusMessage(data.message) // Append our message
+      setFormLoading(false) // Remove the loading state
+    })
+    .catch(err => {
+      console.error(err)
+      setStatusMessage(err.message) // display an error message
+      setFormLoading(false)
+    })
+  }
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Next.js Nodemailer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Next.js - Nodemailer 
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+        <h2>
+          SMTP form submission example
+        </h2>
+        <p>
+          A simple example showcasing a form submission to a serverless API utilising <i><a href="https://nodemailer.com/about/">nodemailer</a></i>.<br/>
+          The example uses an <b><a href="https://ethereal.email/">Ethereal Mail</a></b> set-up for example purposes.
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className={styles.inner}>
+          { formLoading 
+            ? (<p>Loading ... </p>)
+            : (
+              <>
+                <FormComponent submissionHandler={handleSubmission} styles={styles.form} />
+                <div>
+                  <pre>{statusMessage}</pre>
+                </div>
+              </>
+            )
+          }
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
+  )
+}
+/**
+ * A simple form component which can be modifed to include more detail.
+ * For a more complete solution, you may want to return a formData(myForm) object.
+ * However, this will require the appropriate parsing on your serverless function 
+ * to read your form data in a more readable format, e.g. JSON.
+ */
+function FormComponent(props) {
+  const { submissionHandler, styles } = props
+  return (
+    <form id="emailForm" className={styles} method='POST' onSubmit={e => submissionHandler(e)}>
+      <label>Name</label>
+      <input id="emailFormName" name='name' type='text'/>
+      <label>Email</label>
+      <input id="emailFormEmail" required name='email' type='email'/>
+      <input className="button" value="Submit" type='submit' />
+    </form>
   )
 }
